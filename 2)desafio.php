f9915fe9e999fe1c5bed57d483a6b200082410ad
Dayane18 <pdayane2734@gmail.com>
2023-03-29 14:07:02 -0300

    DesafioTarget

---
+++ 2)desafio.php
@@ -0,0 +1,21 @@
+<?php
+$num = 13; // número que será verificado na sequência de Fibonacci
+
+$a = 0;
+$b = 1;
+$c = 0;
+
+while($c < $num){
+    $c = $a + $b;
+    $a = $b;
+    $b = $c;
+}
+
+if($c == $num){
+    echo $num . " pertence à sequência de Fibonacci";
+} else {
+    echo $num . " não pertence à sequência de Fibonacci";
+}
+?>
+
+
